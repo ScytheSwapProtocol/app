d7cdf9b0a3e6f4edd6e628a974df3aea04bd4899
Eric Rice <ericmichaelrice@protonmail.com>
2021-04-26 18:24:22 -0400

    remove border

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { BuyOrSellButton } from "../button/BuyOrSell";
 
 const Wrapper = styled.div`
-  border: 2px solid red;
   display: flex;
   justify-content: center;
   height: 100%;
